3eb05ebfe0fee97dca37b3a2ba50c61d65fcc6e9
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-26 22:26:52 +0400

    wip: add header.

--- components/calendar/CalendarWrapper.tsx
+++ components/calendar/CalendarWrapper.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import { useAppSelector } from "@/redux/hooks";
+import { currentCalendar } from "@/redux/calendar/calendarSlice";
+
+import { Header } from "./Header";
+
 export const CalendarWrapper = () => {
-  return <div>CalendarWrapper</div>;
+  const { year, month } = useAppSelector(currentCalendar);
+  const [isSideCalendar, setIsSideCalendar] = useState<boolean>(true);
+
+  return (
+    <div className="bg-white">
+      <Header
+        year={year}
+        month={month}
+        isSideCalendar={isSideCalendar}
+        setIsSideCalendar={setIsSideCalendar}
+      />
+    </div>
+  );
 };
---
+++ components/calendar/Header.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Dispatch, SetStateAction } from "react";
+
+import { lastWeek, nextWeek, selectDay } from "@/redux/calendar/calendarSlice";
+import { IconLeftArrow, IconMenu, IconRightArrow } from "@/components/icons";
+import { useAppDispatch } from "@/redux/hooks";
+
+export const Header = ({
+  year,
+  month,
+  isSideCalendar,
+  setIsSideCalendar,
+}: {
+  year: number;
+  month: number;
+  isSideCalendar: boolean;
+  setIsSideCalendar: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const dispatch = useAppDispatch();
+  return (
+    <header className="flex h-14 w-full items-center justify-between border-b border-gray-300 px-2 text-black">
+      <div className="flex items-center">
+        <div
+          className="flex cursor-pointer items-center rounded-full p-2 hover:bg-slate-100"
+          onClick={() => setIsSideCalendar(!isSideCalendar)}
+        >
+          <IconMenu className="size-7" />
+        </div>
+        <div className="ml-3 flex items-center">
+          <h1 className="ml-2 hidden text-lg text-gray-500 md:block">
+            კალენდარი
+          </h1>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <button
+          className="mx-3 rounded border border-gray-200 px-3 py-1 text-sm hover:opacity-70"
+          onClick={() => dispatch(selectDay(new Date().toString()))}
+        >
+          დღევანდელი დღე
+        </button>
+
+        <div
+          className="cursor-pointer hover:opacity-40"
+          onClick={() => dispatch(lastWeek())}
+        >
+          <IconLeftArrow className="size-7" />
+        </div>
+        <div
+          className="cursor-pointer hover:opacity-40"
+          onClick={() => dispatch(nextWeek())}
+        >
+          <IconRightArrow className="size-7" />
+        </div>
+        <span className="ml-3 text-sm md:text-lg">
+          {year}წელი {month}თვე
+        </span>
+      </div>
+    </header>
+  );
+};
