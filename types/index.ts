8849a0d731b76160f1a75e12a412906a20bd83dd
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-27 15:55:04 +0400

    chore: add some types.

--- types/index.ts
+++ types/index.ts
@@ -7,3 +7,27 @@ export type IDays = {
   isThisMonth: boolean;
   day: string;
 };
+
+export type IHours = {
+  text: string;
+  hour: number;
+};
+
+export type ITime = { hour: number; minute: number };
+
+export type IRangeColor =
+  | "red"
+  | "orange"
+  | "green"
+  | "blue"
+  | "brown"
+  | "pink";
+
+export type IScheduleDetail = {
+  start: ITime;
+  end: ITime;
+  color: IRangeColor;
+  title: string;
+};
+
+export type ISchedule = { [key: string]: Array<IScheduleDetail> }
