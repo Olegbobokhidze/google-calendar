2c56ab7690a3f273d02941c5c3ab15788a02ad9a
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-27 15:56:33 +0400

    chore: import new comps...

--- components/calendar/CalendarWrapper.tsx
+++ components/calendar/CalendarWrapper.tsx
@@ -7,10 +7,13 @@ import { currentCalendar } from "@/redux/calendar/calendarSlice";
 import { Header } from "./Header";
 import { SideCalendar } from "./SideCalendar";
 import { SideCalendarTitle } from "./SideCalendarTitle";
+import { AddScheduleButton } from "./AddScheduleButton";
+import { AddScheduleModal } from "./AddScheduleDropdown";
 
 export const CalendarWrapper = () => {
   const { year, month, days } = useAppSelector(currentCalendar);
   const [isSideCalendar, setIsSideCalendar] = useState<boolean>(true);
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
 
   return (
     <div className="bg-white">
@@ -21,6 +24,17 @@ export const CalendarWrapper = () => {
         setIsSideCalendar={setIsSideCalendar}
       />
       <main className="flex h-[calc(100%_-_3.5rem)] flex-1">
+        <AddScheduleButton
+          isSideCalendar={isSideCalendar}
+          isOpenModal={isOpenModal}
+          setIsOpenModal={setIsOpenModal}
+        />
+        <AddScheduleModal
+          defaultDate={new Date().toString()}
+          timeIndex={0}
+          isOpen={isOpenModal}
+          setIsOpen={setIsOpenModal}
+        />
         <div
           className={`mt-[65px] flex flex-col p-5 ${
             isSideCalendar ? "block" : "hidden"
