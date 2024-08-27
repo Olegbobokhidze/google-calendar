15894816de121c3b0d15145f1dd58c03ed56e612
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-27 15:55:51 +0400

    chore: add addSchedule

--- redux/schedule/scheduleSlice.ts
+++ redux/schedule/scheduleSlice.ts
@@ -1,13 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "@/redux/store";
+import { ISchedule, IScheduleDetail } from "@/types";
+
+const initialState: ISchedule = {
+  "2022-01-01": [
+    {
+      start: { hour: 1, minute: 20 },
+      end: { hour: 1, minute: 40 },
+      color: "pink",
+      title: "სახელი",
+    },
+  ],
+};
 
 export const scheduleSlice = createSlice({
   name: "schedule",
-  initialState: {},
-  reducers: {},
+  initialState: initialState,
+  reducers: {
+    addSchedule: (
+      state,
+      action: PayloadAction<{ date: string; data: IScheduleDetail }>,
+    ) => {
+      if (!state[action.payload.date]) {
+        state[action.payload.date] = [];
+      }
+      state[action.payload.date] = [
+        ...state[action.payload.date],
+        action.payload.data,
+      ];
+    },
+  },
 });
 
-export const {} = scheduleSlice.actions;
+export const { addSchedule } = scheduleSlice.actions;
 export const schedules = (state: RootState) => state.schedule;
 
 export default scheduleSlice.reducer;
