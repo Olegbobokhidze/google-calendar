33f32b7b580c04f2a5619ba19c4351c7aeec2c8f
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-08-29 17:21:58 +0400

    wip: add schedule calendar.

---
+++ components/calendar/ScheduleCalendar.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { removeSchedule, schedules } from "@/redux/schedule/scheduleSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+
+import { IDays } from "@/types";
+
+import { dayOfWeek, hours24 } from "@/helpers";
+
+type Props = {
+  days: IDays[];
+  setModalDate: React.Dispatch<React.SetStateAction<string>>;
+  setTimeIndex: React.Dispatch<React.SetStateAction<number>>;
+  setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  isDeleteOpen: boolean;
+  setIsDeleteOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const ScheduleCalendar: React.FC<Props> = ({
+  days,
+  setModalDate,
+  setTimeIndex,
+  setIsOpenModal,
+  isDeleteOpen,
+  setIsDeleteOpen,
+}) => {
+  const dispatch = useAppDispatch();
+  const scheduleData = useAppSelector(schedules);
+  const [deleteBox, setDeleteBox] = useState<{ top: number; left: number }>({
+    top: 100,
+    left: 100,
+  });
+  const [deleteSchedule, setDeleteSchedule] = useState<{
+    date: string;
+    index: number;
+  }>({
+    date: "",
+    index: 0,
+  });
+
+  const modalHandle = (date: string, hour: number) => {
+    setModalDate(date);
+    setTimeIndex(hour);
+    setIsOpenModal(true);
+    setIsDeleteOpen(false);
+  };
+
+  const scheduleHandle = (
+    cursor: { top: number; left: number },
+    scheduleData: { date: string; index: number },
+  ) => {
+    setIsOpenModal(false);
+    setIsDeleteOpen(true);
+    setDeleteBox(cursor);
+    setDeleteSchedule(scheduleData);
+  };
+
+  const deleteHandle = () => {
+    setIsDeleteOpen(false);
+    dispatch(
+      removeSchedule({
+        date: deleteSchedule.date,
+        index: deleteSchedule.index,
+      }),
+    );
+  };
+
+  useEffect(() => {
+    if (isDeleteOpen) {
+      document.getElementById("schedule")!.style.overflow = "hidden";
+    } else {
+      document.getElementById("schedule")!.style.overflow = "auto";
+    }
+  }, [isDeleteOpen]);
+
+  return (
+    <>
+      <div className="mb-2 flex w-full flex-col overflow-auto" id="schedule">
+        <div className="flex flex-1 flex-col">
+          <div className="sticky top-0 z-20 flex bg-white">
+            <div className="w-[70px] min-w-[70px] bg-white" />
+            {days.map((day, index) => (
+              <div
+                className="z-20 flex min-w-[81px] flex-1 flex-col bg-white pt-4"
+                key={day.date}
+              >
+                <div className="text-center text-sm font-light">
+                  {dayOfWeek[index]}
+                </div>
+                <div className="p-1 text-center text-2xl font-light">
+                  <div
+                    className={`m-auto flex h-10 w-10 items-center justify-center rounded-full ${day.isToday && "bg-blue-500 text-white"}`}
+                  >
+                    {day.date}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-1">
+            <div className="sticky left-0 top-0 z-10 w-20 min-w-[70px] bg-white">
+              {hours24.map((hour, index) => (
+                <div
+                  className="h-[60px] pr-2 text-right text-[12px] font-light"
+                  key={index}
+                >
+                  {hour.text} საათი
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-1 pt-2">
+              {days.map((day, index) => (
+                <div
+                  className="relative flex min-w-[81px] flex-1 flex-col"
+                  key={`scheduleline-${day.day}`}
+                >
+                  {hours24.map((hour, index) => (
+                    <div
+                      key={`schedule-${index}`}
+                      className="h-[60px] border border-solid border-transparent border-r-zinc-200 border-t-zinc-200"
+                      onClick={() => modalHandle(day.day, index * 4)}
+                    />
+                  ))}
+                  {scheduleData[day.day] && (
+                    <>
+                      {scheduleData[day.day].map((s, idx) => {
+                        if (!s) return;
+
+                        const t = s.start.hour * 60 + s.start.minute;
+                        const top = `${t}px`;
+                        let h =
+                          (s.end.hour - s.start.hour) * 60 -
+                          s.start.minute +
+                          s.end.minute;
+                        if (h < 20) h = 20;
+                        const height = `${h}px`;
+                        return (
+                          <div
+                            key={idx}
+                            className="scheduleBox absolute left-0 w-5/6 overflow-y-auto rounded p-[2px] text-[12px] font-light text-white"
+                            style={{
+                              top: top,
+                              height: height,
+                              background: s.color,
+                            }}
+                            data-schedule={{ date: day.day, index: idx }}
+                            onClick={(e) => {
+                              scheduleHandle(
+                                { top: e.clientY, left: e.clientX },
+                                { date: day.day, index: idx },
+                              );
+                            }}
+                          >
+                            {s.title}
+                          </div>
+                        );
+                      })}
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      {isDeleteOpen && (
+        <div
+          className="fixed z-10 cursor-pointer rounded bg-white px-6 py-2 text-[12px] shadow"
+          style={{ top: `${deleteBox.top}px`, left: `${deleteBox.left}px` }}
+          onClick={() => deleteHandle()}
+        >
+          წაშლა
+        </div>
+      )}
+    </>
+  );
+};
