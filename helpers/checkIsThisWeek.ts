984325d9609199f4f61effeb2a9ce046beb5a92c
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-09-02 16:42:14 +0400

    chore: make more readable helper fn

--- helpers/checkIsThisWeek.ts
+++ helpers/checkIsThisWeek.ts
@@ -3,16 +3,19 @@ export const checkIsThisWeek = (day: Date, current: Date): boolean => {
   const currentYear = current.getFullYear();
   const currentMonth = current.getMonth();
   const currentDay = current.getDay();
+
   const firstDayOfThisWeek = new Date(
     currentYear,
     currentMonth,
     currentDate - currentDay,
   ).getTime();
+
   const lastDayOfThisWeek = new Date(
     currentYear,
     currentMonth,
     currentDate - currentDay + 6,
   ).getTime();
+
   return (
     firstDayOfThisWeek <= day.getTime() && day.getTime() <= lastDayOfThisWeek
   );
